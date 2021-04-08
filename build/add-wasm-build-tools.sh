846f94eb3125043bcb73a590e8c673f4c77f3326
Mokhtar Naamani <mokhtar.naamani@gmail.com>
2021-04-08 18:16:44 +0400

    nightly-2021-03-24

--- build/add-wasm-build-tools.sh
+++ build/add-wasm-build-tools.sh
@@ -4,5 +4,5 @@ set -eu
 
 # Install rust nightly toolchain and wasm target
 # $HOME/.cargo/bin/rustup update nightly
-$HOME/.cargo/bin/rustup install nightly-2021-02-20 --force
-$HOME/.cargo/bin/rustup target add wasm32-unknown-unknown --toolchain nightly-2021-02-20
+$HOME/.cargo/bin/rustup install nightly-2021-03-24 --force
+$HOME/.cargo/bin/rustup target add wasm32-unknown-unknown --toolchain nightly-2021-03-24
